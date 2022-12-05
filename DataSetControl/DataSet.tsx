ab71372391e4f52d428155fc1cc30d1a2e4a5725
Ali Taggaz <ataggaz@hsdyn.com>
2022-12-05 17:31:50 +0100

    add sorting columns

--- DataSetControl/DataSet.tsx
+++ DataSetControl/DataSet.tsx
@@ -1,36 +1,54 @@
 import * as React from 'react';
-import {useState, useEffect, useMemo} from 'react';
-import {DetailsList, Spinner, SpinnerSize } from '@fluentui/react';
+import {useState, useEffect, useMemo, useCallback} from 'react';
+import { DetailsList, IColumn, Spinner, SpinnerSize } from '@fluentui/react';
 import {IDataSetProps} from '../index.types'
 
 
-const DataSet = ({dataset}: IDataSetProps) => {
+const DataSet = ({dataset, columnsHasPercentage}: IDataSetProps) => {
 
-  const [items, setItems] = useState<any>([]);
-  const [columns, setColumns] = useState<any>([]);
+  const [items, setItems] = useState<any[]>([]);
+  const [columns, setColumns] = useState<IColumn[]>([]);
   const [groups, setGroups] = useState<any>([]);
 
   const [loading, setLoading] = useState<boolean>(true);
 
+
+  
+
   useEffect(() => {
     setLoading(dataset.loading); 
+    
     if(!loading) return;
 
-    setColumns(dataset.columns.sort((column1, column2) => column1.order - column2.order).map((column) => {
-      return {
-        name: column.displayName,
-        fieldName: column.name,
-        minWidth: column.visualSizeFactor,
-        key: column.name,
-        type: column.dataType,
-      }
+  setColumns(dataset.columns.sort((column1, column2) => column1.order - column2.order).map((column) => {
+    return {
+      name: column.displayName,
+      fieldName: column.name,
+      minWidth: column.visualSizeFactor,
+      key: column.name,
+      type: column.dataType,
+      isSorted: true,
+      isSortedDescending: false,
+      sortAscendingAriaLabel: 'Sorted A to Z',
+      sortDescendingAriaLabel: 'Sorted Z to A',
+      isGroupable: true,
+      isPadded: true,
+      isResizable: true,
+      isCollapsible: true,
+      isMultiline: true,
+      isRowHeader: true,
     }
-    ));
+  }));
+
+    // split string by comma to get array of group names
+    const fieldInGroup = columnsHasPercentage?.split(',') as string[];
 
     setItems(dataset.sortedRecordIds.map((id) => {
       const entityId = dataset.records[id];
       const attributes = dataset.columns.map((column) => {
-        if(column.dataType === 'Decimal') {
+        for(let field in fieldInGroup)
+        if(fieldInGroup[field] === column.name) {
+          console.info("field from input", fieldInGroup[field], "field from grid", column.name)
           return {[column.name] : (Number(entityId.getFormattedValue(column.name)) * 100).toFixed(7) + '%'};
         } 
         return {[column.name]: entityId.getFormattedValue(column.name)}
@@ -44,30 +62,13 @@ const DataSet = ({dataset}: IDataSetProps) => {
       ...attributes)
     }).sort((a, b) => a.parentId ? -1 : a.parentId < b.parendId ? 1 : 0));
 
-  }, [dataset]);
+  }, [dataset, columns]);
 
-  useMemo(() => {
-    if(!loading) return;
-    const myGroups = items.reduce((prev: any, current: any, currentIndex: number) => {
-      if(prev.lenght === 0 || prev[prev.lenght - 1].key !== current.parentId) {
-        prev.push({
-          key: current.parentId,
-          raw: current,
-          startIndex: currentIndex,
-          name: current.raw.getFormattedValue("accountid"),
-          parentId: current.parentId,
-          count: 1,
-          level: 0,
-        })
-      } else {
-        prev[prev.lenght - 1].count++;
-      }
-      return prev;
-    }, []);
-    setGroups(myGroups);
-  }, [items])
 
-console.log("column",columns);
+
+
+
+console.log("colums",columns);
 console.log("items",items);
 console.log("loading",loading);
 console.log("groups",groups);
@@ -79,7 +80,8 @@ console.log("dataset",dataset);
         loading ? <Spinner label="Loading..." size={SpinnerSize.large} /> : 
         <DetailsList 
           items={items} 
-          columns={columns} 
+          columns={columns}
+          styles={{root: {overflow: 'auto', height: '100%', width: '100%'}}}
           
         />
       }
